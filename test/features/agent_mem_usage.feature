4ae9f73f8149f7588179af6456112cd695b4e55f
Ari Hyttinen <ext-ari.hyttinen@nokia.com>
2010-09-16 11:07:02 +0300

    Added SUT::agent_mem_usage test

---
+++ test/features/agent_mem_usage.feature
@@ -0,0 +1,9 @@
+@linux, @windows, @symbian, @meego
+Feature: SUT::agent_mem_usage should return positive integer
+	As a test scripter writer
+	I want to see how much memory the TDriver agent process is using at the target device
+	so that I can test verify that memory problems are not caused by it.
+
+	Scenario: Query TDriver mem usage of TDriver agent
+	        When I execute "@sut_agent_mem_usage = @sut.agent_mem_usage"
+	        Then mem usage result is a positive integer
---
+++ test/features/step_definitions/agent_mem_usage.rb
@@ -0,0 +1,44 @@
+############################################################################
+## 
+## Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies). 
+## All rights reserved. 
+## Contact: Nokia Corporation (testabilitydriver@nokia.com) 
+## 
+## This file is part of TDriver. 
+## 
+## If you have questions regarding the use of this file, please contact 
+## Nokia at testabilitydriver@nokia.com . 
+## 
+## This library is free software; you can redistribute it and/or 
+## modify it under the terms of the GNU Lesser General Public 
+## License version 2.1 as published by the Free Software Foundation 
+## and appearing in the file LICENSE.LGPL included in the packaging 
+## of this file. 
+## 
+############################################################################
+
+
+# The testapp application example must be compiled and in PATH for this test to work
+# sut_qt1, sut_qt2, sut_qt3 and sut_qt4 suts must be defined in tdriver_parameters.xml
+# /tdriver/Tutorials/QT/application_examples/calculator/release
+# Author: Ari Hyttinen
+# Date: 16.09.2010
+# Purpose: Tests TDriver Qt methods
+
+
+require 'tdriver'
+include TDriverVerify
+
+Before do
+	$ErrorMessage=""
+end
+
+
+Then /^mem usage result is a positive integer$/ do
+  verify_true(0, "SUT::agent_mem_usage should return class Fixnum, but it returned class #{@sut_agent_mem_usage.class}") {
+    @sut_agent_mem_usage.class == Fixnum
+  }
+  verify_true(0, "SUT::agent_mem_usage should return positive value, but it returned #{@sut_agent_mem_usage}") {
+    @sut_agent_mem_usage > 0
+  }
+end
