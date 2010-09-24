7d9860b6240be7efbf2cab9a8b94ed655f5b7d6f
Olli Ruotsalainen <ext-olli.1.ruotsalainen@nokia.com>
2010-09-24 13:32:44 +0300

    Added new common steps and features for set attribute.

--- test/features/qt_attribute_set_attribute.feature
+++ test/features/qt_attribute_set_attribute.feature
@@ -1,15 +1,16 @@
 Feature: MobyBehaviour::QT::Attribute#set_attribute
   As a test scripter writer
-  I want to use set_attribute method to [DO_SOMETHING] in [TARGET_APPLICATION]
+  I want to use set_attribute method to set object attributes in testapp
   so that I can test the MobyBehaviour::QT::Attribute behaviour
 
-  Scenario: Testing set_attribute method with required argument(s) (Rename this to be more descriptive)
-    Given I launch application [APPLICATION_NAME] as @app
-    When I execute "@app.[SOME_OBJECT].set_attribute(attribute, value)"
-    Then [ADD_YOUR_VERIFICATION_HERE]
+  Scenario: Testing set_attribute method with required arguments 
+    Given I launch application "testapp" as "@app"
+    When I execute "@app.Control( :name => 'Crash' ).set_attribute('x', '150')"
+    Then the Control with name Crash has the attribute x with the value 150
 
-  Scenario: Testing set_attribute method with optional argument 'type' (Rename this to be more descriptive)
-    Given I launch application [APPLICATION_NAME] as @app
-    When I execute "@app.[SOME_OBJECT].set_attribute(attribute, value, type)"
-    Then [ADD_YOUR_VERIFICATION_HERE]
+  Scenario: Testing set_attribute method with optional argument 'type'
+    Given I launch application "testapp" as "@app"
+    When I execute "@app.Control(:name => 'Crash').set_attribute('pos', '200,250', 'QPoint')"    
+	Then the Control with name Crash has the attribute x with the value 200
+	And the Control with name Crash has the attribute y with the value 250
 
--- test/features/step_definitions/feature_common.rb
+++ test/features/step_definitions/feature_common.rb
@@ -82,6 +82,38 @@ Given /^I show FloatingMenu of the testapp$/ do
   @__current_app.ControlTab( :name => 'ControlTab').drag(:Up, 10)
 end
 
+Given "I know the $target_type initial location" do | target_type |
+
+  temp_obj = @__current_app.child( :type => target_type )
+  
+  @initial_x = temp_obj.attribute( "x" )
+  @initial_y = temp_obj.attribute( "y" )
+
+
+end
+
+
+Given "there is only $article $target_type on the testapp screen" do | article, target_type |
+
+  @__current_app.ControlTab( :name => 'ControlTab').drag(:Up, 10)
+   
+  @__current_app.Control( :name => "Reset" ).tap
+  case target_type
+    when "Node"
+	  @__current_app.Control( :name => "AddNode" ).tap  
+	when "Rectangle"
+	  @__current_app.Control( :name => "AddRectangle" ).tap  	
+  end
+  
+  temp_obj = @__current_app.child( :type => target_type )
+  
+  @initial_x = temp_obj.attribute( "x" )
+  @initial_y = temp_obj.attribute( "y" )
+  
+  @__current_app.ControlTab( :name => 'ControlTab').drag(:Down, 10)
+    
+end
+
 When /^I execute "([^\"]*)"$/ do |script|
     raise 'Invalid step! In these feature tests there should be only one "When I execute..." example code per scenario.' if @__example_given
     @__example_given = true
@@ -146,4 +178,41 @@ end
 
 Then /^exception is thrown$/ do
   verify_false(0, "Exception has not been raised") { @__exception.nil? }
+end
+
+Then "the $target_type has moved $expected_direction" do | target_type, expected_direction |
+
+  verify_true(30, "The #{target_type} did not move #{expected_direction}") do
+    
+	result = false
+	temp_obj = @__current_app.child( :type => target_type )
+    case expected_direction
+      when "left"
+	    result = temp_obj.attribute( "x" ) < @initial_x
+	  when "right"
+	    result = temp_obj.attribute( "x" ) > @initial_x
+      when "down"
+	    result = temp_obj.attribute( "y" ) > @initial_y
+	  when "up"
+	    result = temp_obj.attribute( "y" ) < @initial_y
+    end  
+	
+	result
+  
+  end
+  
+end
+
+Then "the $target_type with $id_type $id_value has the attribute $expected_attribute with the value $expected_value" do | target_type, target_attribute, target_value, expected_attribute, expected_value |
+
+  verify_equal(expected_value, 30, "The #{target_type} did not have the #{expected_attribute} attribute with a value of #{ expected_value }") do
+    
+	result = false
+	temp_obj = @__current_app.child( :type => target_type, target_attribute.to_sym => target_value )
+    
+	temp_obj.attribute( expected_attribute )
+	
+  end
+
+
 end
