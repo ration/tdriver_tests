db7fc0c093574a672a72cfd9c86b7dbf211a7b6d
Ivan Castane Capel <ivacasta@.NOE.Nokia.com>
2010-10-25 18:35:36 +0100

    Added tests for localisation table creation

---
+++ test/test_data/settings_en.ts
@@ -0,0 +1,56 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en_US">
+<context>
+    <name></name>
+    <message>
+        <source>qtn_sett_launcher</source>
+        <translation variants="yes">
+            <lengthvariant>Settings</lengthvariant>
+            <lengthvariant>Sett.</lengthvariant>
+        </translation>
+    </message>
+    <message>
+        <source>qtn_sett_main_title</source>
+        <translation variants="yes">
+            <lengthvariant>Settings</lengthvariant>
+            <lengthvariant>Sett.</lengthvariant>
+        </translation>
+    </message>
+    <message>
+        <source>qtn_sett_main_other</source>
+        <translation>All settings</translation>
+    </message>
+    <message>
+        <source>qtn_sett_main_utilities</source>
+        <translation>Utilities</translation>
+    </message>
+    <message>
+        <source>qtn_sett_main_data</source>
+        <translation>Device system</translation>
+    </message>
+    <message>
+        <source>qtn_sett_main_look</source>
+        <translation variants="yes">
+            <lengthvariant>Look and feel</lengthvariant>
+            <lengthvariant>Look &amp; feel</lengthvariant>
+        </translation>
+    </message>
+    <message>
+        <source>qtn_sett_main_most</source>
+        <translation>Recently used</translation>
+    </message>
+    <message>
+        <source>qtn_sett_main_region</source>
+        <translation>Time and language</translation>
+    </message>
+    <message>
+        <source>qtn_sett_main_connectivity</source>
+        <translation>Connectivity</translation>
+    </message>
+    <message>
+        <source>qtn_sett_main_application</source>
+        <translation>Application settings</translation>
+    </message>
+</context>
+</TS>
