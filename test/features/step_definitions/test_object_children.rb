f67aec2c83bcb7f45f2dc364c7f1ac52ef83f200
bgargadi <bilkis.gargadia@nokia.com>
2010-10-25 15:00:17 +0100

    Adding tests for test object children method

---
+++ test/features/step_definitions/test_object_children.rb
@@ -0,0 +1,28 @@
+############################################################################
+## 
+## Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies). 
+## All rights reserved. 
+## Contact: Nokia Corporation (testabilitydriver@nokia.com) 
+## 
+## This file is part of TDriver. 
+## 
+## If you have questions regarding the use of this file, please contact 
+## Nokia at testabilitydriver@nokia.com . 
+## 
+## This library is free software; you can redistribute it and/or 
+## modify it under the terms of the GNU Lesser General Public 
+## License version 2.1 as published by the Free Software Foundation 
+## and appearing in the file LICENSE.LGPL included in the packaging 
+## of this file. 
+## 
+############################################################################
+
+
+# Author: Bilkis Gargadia
+# Date: 25.10.2010
+# Purpose: Test TDriver methods
+
+Then ("an array of test objects of size \"$size\" is returned" ) do |$size|
+  raise @__exception if @__exception != nil
+  verify_true(0, 'Test objects were not found by children method' ) { $test_objs.class == Array && $test_objs.size==$size.to_i && $test_objs[0].class == MobyBase::TestObject}
+end
