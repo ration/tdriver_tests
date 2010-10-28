c33c731b3f24fb0fa21744dd83b0237afb9a0145
Jussi Koljonen <ext-jussi.koljonen@nokia.com>
2010-10-28 16:36:44 +0300

    tests#rakefile.rb: refactored

--- rakefile.rb
+++ rakefile.rb
@@ -1,42 +1,36 @@
 task :default do
 
-  abort("Supported tasks: doc")
+  abort("Supported tasks: doc_linux, doc_windows")
+
+end
+
+def run_tests( name, command_line )
+
+  puts "Executing documentation #{ name } feature tests...\n"
+
+  begin
+
+    Dir.chdir("test")
+
+    system( command_line )
+
+  ensure
+
+    Dir.chdir("..")
+
+  end
 
 end
 
 task :doc_linux do 
 
-  puts "Executing documentation linux feature tests...\n"
-
-  begin
-
-    Dir.chdir("test")
-
-    system("cucumber features -f TDriverDocument::CucumberReport -f TDriverReport::CucumberReporter --out log.log --tags @qt_linux ")
-
-  ensure
-
-    Dir.chdir("..")
-
-  end
+  run_tests( "linux", "cucumber features -f TDriverDocument::CucumberReport -f TDriverReport::CucumberReporter --out log.log --tags @qt_linux" )
 
 end
 
 task :doc_windows do 
 
-  puts "Executing documentation windows feature tests...\n"
-
-  begin
-
-    Dir.chdir("test")
-
-    system("cucumber features -f TDriverDocument::CucumberReport -f TDriverReport::CucumberReporter --out log.log --tags @qt_windows ")
-
-  ensure
-
-    Dir.chdir("..")
-
-  end
+  run_tests( "windows", "cucumber features -f TDriverDocument::CucumberReport -f TDriverReport::CucumberReporter --out log.log --tags @qt_windows" )
 
 end
 
