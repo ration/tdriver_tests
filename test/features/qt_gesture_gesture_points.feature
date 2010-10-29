24bfc4d12c947eda96a79e10fb02a4034f0ff3dd
Mika Leiman <ext-mika.1.leiman@nokia.com>
2010-10-29 13:26:41 +0300

    Updated gesture poinsts feature test

--- test/features/qt_gesture_gesture_points.feature
+++ test/features/qt_gesture_gesture_points.feature
@@ -1,11 +1,23 @@
 Feature: MobyBehaviour::QT::Gesture#gesture_points
   As a test scripter writer
-  I want to use gesture_points method to [DO_SOMETHING] in [TARGET_APPLICATION]
+  I want to use gesture_points method to use recorded gesture motions
   so that I can test the MobyBehaviour::QT::Gesture behaviour
 
-  Scenario: Testing gesture_points method with required argument(s) (Rename this to be more descriptive)
-    Given I launch application [APPLICATION_NAME] as @app
-    When I execute "@app.[SOME_OBJECT].gesture_points(points, duration)"
+  Scenario: Gesture points to given object using default values
+    Given I have default sut
+    And I launch application "testapp"
+    And I hide the triangle
+    And I drag node 1 to the center of the screen
+    When I execute 
+               """
+                @app.Node(:name => @nodeid).gesture_points([
+		{'x' => @x.to_i, 'y' => @y.to_i, 'interval' => 1.828 },
+		{'x' => 455, 'y' => 268, 'interval' => 0.172 },
+		{'x' => 456, 'y' => 269, 'interval' => 0.0 },
+		{'x' => 457, 'y' => 269, 'interval' => 0.016 },
+		{'x' => 459, 'y' => 270, 'interval' => 0.0 },
+		{'x' => 461, 'y' => 270, 'interval' => 0.016 }],4.61)
+               """
     Then [ADD_YOUR_VERIFICATION_HERE]
 
   Scenario: Testing gesture_points method with optional argument 'mouse_details' (Rename this to be more descriptive)
---
+++ test/features/step_definitions/qt_gesture_points.rb
@@ -0,0 +1,51 @@
+############################################################################
+##
+## Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
+## All rights reserved.
+## Contact: Nokia Corporation (testabilitydriver@nokia.com)
+##
+## This file is part of TDriver.
+##
+## If you have questions regarding the use of this file, please contact
+## Nokia at testabilitydriver@nokia.com .
+##
+## This library is free software; you can redistribute it and/or
+## modify it under the terms of the GNU Lesser General Public
+## License version 2.1 as published by the Free Software Foundation
+## and appearing in the file LICENSE.LGPL included in the packaging
+## of this file.
+##
+############################################################################
+
+
+# Common step definitions to be used in feature testing of TDriver
+# Author: Mika Leiman
+# Date: 29.10.2010
+# Purpose: Steps used to test gesture points behaviour
+
+Then("I drag node $number to the center of the screen") do |$number|
+  verify(30) {
+  window_width = @sut.application.NodeView.attribute('width').to_i/2
+  window_height = @sut.application.NodeView.attribute('height').to_i/2
+  window_x = @sut.application.NodeView.attribute('x_absolute').to_i + window_width.to_i
+  window_y = @sut.application.NodeView.attribute('y_absolute').to_i + window_height.to_i
+  @nodeid = "Node"+$number.to_s
+  @sut.application.Node(:name => @nodeid).drag_to(window_x.to_i, window_y.to_i)
+  node_width = @sut.application.Node(:name => @nodeid).attribute('width').to_i/2
+  node_height = @sut.application.Node(:name => @nodeid).attribute('height').to_i/2
+  new_x = @sut.application.Node(:name => @nodeid).attribute('x').to_i+node_width.to_i
+  new_y = @sut.application.Node(:name => @nodeid).attribute('y').to_i+node_height.to_i
+
+  	x_tolerance=false
+  	if window_width.to_i - 5 < new_x.to_i && window_width.to_i + 5 > new_x.to_i
+			x_tolerance=true
+  	end
+  	y_tolerance=false
+  	if window_height.to_i - 5 < new_y.to_i && window_height.to_i + 5 > new_y.to_i
+			y_tolerance=true
+  	end
+  	raise "x tolerance not accepted" unless x_tolerance
+  	raise "y tolerance not accepted" unless y_tolerance
+  }
+end
+
