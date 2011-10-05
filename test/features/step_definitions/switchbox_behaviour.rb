1fa66e8cf9867f67cfbb7b06f0df819ac3e3b741
Jussi Koljonen <ext-jussi.koljonen@nokia.com>
2011-10-05 11:18:03 +0300

    tests#switchbox_behaviour.rb: changed 'cucumber -h' to 'ruby -v'

--- test/features/step_definitions/switchbox_behaviour.rb
+++ test/features/step_definitions/switchbox_behaviour.rb
@@ -30,12 +30,19 @@ include TDriverVerify
 Then "I set the mandatory switchbox parameters for sut" do
   @sut.parameter[:switchbox_sleep_before_powerup_in_reboot]='1'
   @sut.parameter[:switchbox_sleep_after_powerup_in_reboot]='1'
-  @sut.parameter[:switchbox_powerdown_command_sequence]='cucumber --help|cucumber -h'
-  @sut.parameter[:switchbox_powerup_command_sequence]='cucumber -h|cucumber --help'
+  #@sut.parameter[:switchbox_powerdown_command_sequence]='cucumber --help|cucumber -h'
+  #@sut.parameter[:switchbox_powerup_command_sequence]='cucumber -h|cucumber --help'
+
+  @sut.parameter[:switchbox_powerdown_command_sequence]='ruby -v|ruby -v'
+  @sut.parameter[:switchbox_powerup_command_sequence]='ruby -v|ruby -v'
+
   @sut.parameter[:switchbox_timeout_between_command_sequence]='1'
   @sut.parameter[:switchbox_powerdown_command_success_string]='true'
   @sut.parameter[:switchbox_powerup_command_success_string]='true'
-  @sut.parameter[:switchbox_commands_after_powerup_in_reboot]='cucumber --help'
+  #@sut.parameter[:switchbox_commands_after_powerup_in_reboot]='cucumber --help'
+
+  @sut.parameter[:switchbox_commands_after_powerup_in_reboot]='ruby -v'
+
 end
 
 Then "The power status is $power_status" do |power_status|
